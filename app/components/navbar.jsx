baab5365a49f03f3f122db8b57fe38a043e34d5d
Mamun <md0465981@gmail.com>
2023-10-08 21:31:22 +0600

    Added background on navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import style from '@/app/styles/navbar.module.css'
-import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAdd } from '@fortawesome/free-solid-svg-icons'
+import { faAdd, faUserAlt } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+import logo from "@/app/images/logo.png"
 
 function Navbar({ toggleForm }) {
 
@@ -13,7 +14,13 @@ function Navbar({ toggleForm }) {
     return (
         <ul className={style.navbar}>
             <li>
+                <Image src={logo} height={25} width={20} alt='Logo Image' ></Image>
+            </li>
+            <li>
                 <FontAwesomeIcon className={style.nav_icons} icon={faAdd} onClick={handleclick} ></FontAwesomeIcon>
+            </li>
+            <li>
+                <FontAwesomeIcon id={style.user_icon} className={style.nav_icons} icon={faUserAlt}></FontAwesomeIcon>
             </li>
 
         </ul>
